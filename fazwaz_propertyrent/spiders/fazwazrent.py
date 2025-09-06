fb33616d5ee59075bbb5dbb25657e76965a1a2b6
aliepratama <aliepratama2@gmail.com>
2025-09-06 14:57:05 +0700

    feat: add argument option for region and property type command

--- fazwaz_propertyrent/spiders/fazwazrent.py
+++ fazwaz_propertyrent/spiders/fazwazrent.py
@@ -2,15 +2,17 @@ import re
 import scrapy
 
 from fazwaz_propertyrent.items import FazwazPropertyrentItem
+from fazwaz_propertyrent.params import build_start_urls
 
 
 class FazwazRentSpider(scrapy.Spider):
     name = 'fazwazrent'
     allowed_domains = ['fazwaz.my']
 
-    start_urls = [
-        'https://www.fazwaz.my/penthouse-for-rent/malaysia/penang'
-    ]
+    def __init__(self, region=None, property_type=None, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.start_urls = build_start_urls(region, property_type)
+        self.logger.info(f'Starting URLs: {self.start_urls}')
 
     def parse(self, response):
         # Follow links to property detail pages
@@ -39,9 +41,12 @@ class FazwazRentSpider(scrapy.Spider):
 
         item['property_type'] = self._extract_basic_info(
             response, 'Property Type')
-        item['furnished'] = (self._extract_basic_info_alt(response, 'Furniture') or self._extract_basic_info(response, 'Furniture'))
-        item['seller_name'] = (self._extract_basic_info_alt(response, 'Listed By') or self._extract_basic_info(response, 'Listed By'))
-        item['area'] = (self._extract_basic_info(response, 'Size') or self._extract_basic_info(response, 'Plot Size'))
+        item['furnished'] = (self._extract_basic_info_alt(
+            response, 'Furniture') or self._extract_basic_info(response, 'Furniture'))
+        item['seller_name'] = (self._extract_basic_info_alt(
+            response, 'Listed By') or self._extract_basic_info(response, 'Listed By'))
+        item['area'] = (self._extract_basic_info(response, 'Size')
+                        or self._extract_basic_info(response, 'Plot Size'))
 
         item['description'] = self._extract_description(response)
         item['images'] = self._extract_images(response)
@@ -79,7 +84,7 @@ class FazwazRentSpider(scrapy.Spider):
         data = response.xpath(
             f'//span[normalize-space(text())="{attribute_key}"]/following-sibling::span//text()').getall()
         return self._clean_ws(' '.join(data)) or ''
-    
+
     def _extract_basic_info_alt(self, response, attribute_key) -> str:
         # e.g., 'Furnished' field
         data = response.xpath(
